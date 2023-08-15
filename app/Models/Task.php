00541155003b99055e077387a1a40d806e24474d
slaventiyOcem <himenko9595@gmail.com>
2023-08-15 20:50:30 +0300

    model add fillable name

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $fillable = ['name',];
    public function user(){
        return $this->belongsTo(User::class);
    }
