ab3ae6c19b1564734b03925b614c7989792684ea
slaventiyOcem <himenko9595@gmail.com>
2023-08-15 20:51:36 +0300

    view add link to tasks.create

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,14 +8,20 @@
                 <div class="panel-heading">{{ trans('tasks.index.page_name') }}</div>
 
                 <div class="panel-body">
+                    <a href="{{route('tasks.create')}}" class="btn btn-success"><i class="fa fa-plus">Create new task</i></a>
                     <table class="table table striped task_table">
+                        <div class="panel-body">
+
+                        </div>
                         <thead>
+
                         <tr>
                             <th>{{trans('tasks.index.columns.tasks')}}</th>
                             <th>{{trans('tasks.index.columns.action')}}</th>
                         </tr>
                         </thead>
                         <tbody>
+
                         @foreach ($tasks as $task)
                             <tr>
                                 <td class="table-text">
