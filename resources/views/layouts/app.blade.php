a8fce663c573ef59a8e62a61f6c3d2be0eeeb147
slaventiyOcem <himenko9595@gmail.com>
2023-08-15 19:34:02 +0300

    controlle index action

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,8 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ route('tasks.index')
+ }}">Tasks</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
