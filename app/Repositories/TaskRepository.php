65e019f4d0afc054833b52caf1c0f723f72de051
slaventiyOcem <himenko9595@gmail.com>
2023-08-17 19:46:21 +0300

    tasks repository

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+
+class TaskRepository
+{
+    /**
+     * get all tasks for specify
+     * @param User $user
+     * @return \Illuminate\Support\Collection
+     */
+    public function forUser (User $user){
+        return $user->tasks;
+    }
+}
